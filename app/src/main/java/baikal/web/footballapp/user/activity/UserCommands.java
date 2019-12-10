71c3d9f1f00a8b039cc0bc844f4aa1ee45202b17
BrutalBro <forever.looser.17.22.48@gmail.com>
2019-12-10 12:25:08 +0800

    why ?

--- app/src/main/java/baikal/web/footballapp/user/activity/UserCommands.java
+++ app/src/main/java/baikal/web/footballapp/user/activity/UserCommands.java
@@ -114,7 +114,7 @@ public class UserCommands extends Fragment {
         RVUserCommandAdapter adapter = new RVUserCommandAdapter(getActivity(), teams, (t, l)->{
             DialogTeam dialogRegion =  new DialogTeam(i->{
                 if(i==0){
-                    Log.d("UserCommands", " вика получишь щас ");
+                    Log.d("UserCommands", " за что? что я сделал не так? 0_о ");
                     Intent intent = new Intent(getActivity(), TeamMatches.class);
                     Bundle bundle = new Bundle();
                     Bundle bundle1 = new Bundle();
